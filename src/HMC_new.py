0efa42d7567cbde2a465f855f2f79b87c9972425
Pierre Tessier <pierre.borie.tessier@gmail.com>
2022-02-28 17:53:04 -0500

    clean repo and bug fixes

--- src/HMC_new.py
+++ src/HMC_new.py
@@ -24,28 +24,28 @@ class HMC(keras.Model):
         self.model = model
         self.L = L
         self.epsilon = epsilon
-        self.batch_size = batch_size
-        self.param_num = sum([tf.size(var) for var in model.trainable_variables])
+        self.batch_size = tf.constant(batch_size)
+        self.param_num = tf.constant(sum([tf.size(var) for var in model.trainable_variables]))
         self.param_shapes = [var.shape for var in model.trainable_variables]
         self.log_gamma = tf.Variable(tf.random.normal((1,)))
         self.log_lambda = tf.Variable(tf.random.normal((1,)))
-        self.loss = keras.losses.MeanSquaredError()
+        self.loss = model.loss
 
     @staticmethod
     def probability(hamiltonian_init, hamiltonian_final):
         p = tf.exp(hamiltonian_init - hamiltonian_final)
-        p = tf.minimum(p, 1)
+        p = tf.minimum(p, 1.0)
         return p
 
     @staticmethod
     def kinetic_energy(state: HMCState):
-        k = (tf.reduce_sum(state.momentum ** 2) + state.log_gamma_p ** 2 + state.log_lambda_p ** 2) / 2
+        k = (tf.reduce_sum(state.momentum ** 2) + state.log_gamma_p ** 2 + state.log_lambda_p ** 2) / 2.0
         return k
 
     @staticmethod
     def get_hmc_grad(grad, state: HMCState):
         w = state.position
-        dw = tf.exp(state.log_gamma) / 2 * grad + tf.exp(state.log_lambda) * tf.sign(w)
+        dw = tf.exp(state.log_gamma) / 2.0 * grad + tf.exp(state.log_lambda) * tf.sign(w)
         return dw
 
     def get_model_params(self):
@@ -61,15 +61,19 @@ class HMC(keras.Model):
             param = params[idx:(idx+size)]
             param = tf.reshape(param, shape)
             shaped_params.append(param)
-        self.model.trainable_variables.assign(shaped_params)
+        for var, param in zip(self.model.trainable_variables, shaped_params):
+            var.assign(param)
 
     def get_hyper_grad(self, inputs, state: HMCState):
         self.set_model_params(state.position)
         loss = self.get_loss(inputs)
-        dlog_gamma = tf.exp(state.log_gamma) * (loss / 2 + 1) - (self.batch_size / 2 + 1)
-        dlog_lambda = tf.exp(state.log_lambda) * (tf.reduce_sum(tf.abs(state.position)) + 1) - (self.param_num + 1)
+        dlog_gamma = tf.exp(state.log_gamma) * (loss / 2.0 + 1.0) \
+                     - (tf.cast(self.batch_size, tf.float32) / 2.0 + 1.0)
+        dlog_lambda = tf.exp(state.log_lambda) * (tf.reduce_sum(tf.abs(state.position)) + 1.0) \
+                      - (tf.cast(self.param_num, tf.float32) + 1.0)
         return dlog_gamma, dlog_lambda
 
+    @tf.function
     def call(self, inputs):
         momentum = tf.random.normal((self.param_num,))
         log_gamma_p = tf.random.normal((1,))
@@ -86,11 +90,11 @@ class HMC(keras.Model):
         if p < p_new_state:
             final_state = new_state
             self.update_state(final_state)
-            return final_state, loss_final
+            return final_state, loss_final, p_new_state, True, hamiltonian_final
         else:
             final_state = init_state
             self.update_state(init_state)
-            return final_state, loss_initial
+            return final_state, loss_initial, p_new_state, False, hamiltonian_init
 
     def get_config(self):
         # TODO
@@ -113,6 +117,7 @@ class HMC(keras.Model):
         batch, targets = inputs
         pred = self.model(batch)
         loss = self.loss(pred, targets)
+        loss += tf.reduce_sum(self.model.losses)
         return loss
 
     def get_loss_and_grad(self, inputs, state=None):
@@ -123,16 +128,18 @@ class HMC(keras.Model):
             tape.watch(batch)
             pred = self.model(batch)
             loss = self.loss(pred, targets)
-        grad = tape.gradient(loss, self.get_model_params())
+            loss += tf.reduce_sum(self.model.losses)
+        grad = tape.gradient(loss, self.model.trainable_variables)
+        grad = tf.concat([tf.reshape(param, [-1]) for param in grad], 0)
         return loss, grad
 
     def potential_energy(self, inputs, state: HMCState):
         w = state.position
         loss = self.get_loss(inputs)
-        u = tf.exp(state.log_gamma) * (loss / 2 + 1) \
-            + tf.exp(state.log_lambda) * (tf.reduce_sum(tf.abs(w)) + 1) \
-            - (self.batch_size / 2 + 1) * state.log_gamma \
-            - (self.param_num + 1) * state.log_lambda
+        u = tf.exp(state.log_gamma) * (loss / 2.0 + 1.0) \
+            + tf.exp(state.log_lambda) * (tf.reduce_sum(tf.abs(w)) + 1.0) \
+            - (tf.cast(self.batch_size, tf.float32) / 2.0 + 1.0) * state.log_gamma \
+            - (tf.cast(self.param_num, tf.float32) + 1.0) * state.log_lambda
         return u, loss
 
     def hamiltonian(self, inputs, state: HMCState):
@@ -149,9 +156,9 @@ class HMC(keras.Model):
             grad = HMC.get_hmc_grad(grad, current_state)
             dlog_gamma, dlog_lambda = self.get_hyper_grad(inputs, current_state)
 
-            log_gamma_p_new = current_state.log_gamma_p - self.epsilon / 2 * dlog_gamma
-            log_lambda_p_new = current_state.log_lambda_p - self.epsilon / 2 * dlog_lambda
-            momentum_new = current_state.momentum - self.epsilon / 2 * grad
+            log_gamma_p_new = current_state.log_gamma_p - self.epsilon / 2.0 * dlog_gamma
+            log_lambda_p_new = current_state.log_lambda_p - self.epsilon / 2.0 * dlog_lambda
+            momentum_new = current_state.momentum - self.epsilon / 2.0 * grad
 
             log_gamma_new = current_state.log_gamma + self.epsilon * log_gamma_p_new
             log_lambda_new = current_state.log_lambda + self.epsilon * log_lambda_p_new
@@ -168,9 +175,9 @@ class HMC(keras.Model):
             grad = HMC.get_hmc_grad(grad, half_state)
             dlog_gamma, dlog_lambda = self.get_hyper_grad(inputs, half_state)
 
-            log_gamma_p_new = half_state.log_gamma_p - self.epsilon / 2 * dlog_gamma
-            log_lambda_p_new = half_state.log_lambda_p - self.epsilon / 2 * dlog_lambda
-            momentum_new = half_state.momentum - self.epsilon / 2 * grad
+            log_gamma_p_new = half_state.log_gamma_p - self.epsilon / 2.0 * dlog_gamma
+            log_lambda_p_new = half_state.log_lambda_p - self.epsilon / 2.0 * dlog_lambda
+            momentum_new = half_state.momentum - self.epsilon / 2.0 * grad
 
             current_state = HMCState(half_state.position,
                                      half_state.log_gamma,
